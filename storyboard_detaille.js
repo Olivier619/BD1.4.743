681647954c9d1b8bb1ccb01953499df105b67030
Olivier619 <132900963+Olivier619@users.noreply.github.com>
2025-04-01 08:05:18 +0200

    Create storyboard_detaille.js

---
+++ storyboard_detaille.js
@@ -0,0 +1,9 @@
+window.createStoryboardComplet = async function(scenario) {
+    if (!scenario?.chapters) {
+        throw new Error("Scénario invalide");
+    }
+
+    // ... code original avec vérification des entrées ...
+    
+    return storyboardComplet;
+};
